c2006365da6cdfbcb24cdf136d2d06187144619e
Briskye <133975189+Briskye@users.noreply.github.com>
2026-05-20 14:06:27 +0800

    .env file added with API KEY

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 const cors = require("cors");
 const path = require("path");
 
@@ -16,50 +16,40 @@ app.get("/", (req, res) => {
 });
 
 /* EMAIL CONFIG */
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 /* API ROUTE */
 app.post("/send-email", async (req, res) => {
-  console.log("Request received:", req.body);
-  const { name, phone, email, message } = req.body;
 
   try {
-    await transporter.sendMail({
-      from: "martinmendoza03.mm@gmail.com",
+
+    const { name, phone, email, message } = req.body;
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
       to: "john@linkmind.co.jp",
-      subject: "LinkMind Message",
+      subject: "New Contact Form Submission",
 
       html: `
-        <div style="font-family: Arial, sans-serif; font-size: 16px;">
+        <h2>New Contact Message</h2>
 
-          <h2>LinkMind Message</h2>
+        <p><strong>Name:</strong> ${name}</p>
 
-          <p><strong>Name:</strong> ${name}</p>
-          <p><strong>Phone Number:</strong> ${phone}</p>
-          <p><strong>Email:</strong> ${email}</p>
-          <p><strong>Message:</strong></p>
-          <p>${message}</p>
+        <p><strong>Phone Number:</strong> ${phone}</p>
 
-        </div>
+        <p><strong>Email:</strong> ${email}</p>
+
+        <p><strong>Message:</strong></p>
+
+        <p>${message}</p>
       `
     });
 
+    
     res.status(200).json({ success: true });
 
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false });
   }
-});
-    
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
